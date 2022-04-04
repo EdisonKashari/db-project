7f75cd14c82470a235f5f21f0c69a9141e94d791
Edison Kashari <edisonkashari@hotmail.com>
2022-04-04 12:41:08 -0400

    first commit

---
+++ Queries/day1.sql
@@ -0,0 +1,22 @@
+--This is oew you right single line comments in sql
+
+/*
+ this is how write multi line comment
+ */
+-- each query you write known as sql statement
+--* means all,here it means all columns
+--from sometable that exist in the database
+-- just like java ,
+--SQL Statements end in semicolon ; and it's mandatory
+SELECT * FROM REGIONS ;
+-- If you want to run second statement
+--you just need to click the query to focus the cursor and click run
+SELECT * FROM COUNTRIES ;
+
+SELECT * FROM LOCATIONS ;
+
+
+
+
+
+
